bfc5c1615023e4d90fcdb67c76aa50034941b434
shialax <74329916+shialax@users.noreply.github.com>
2024-10-28 11:58:49 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
-from flask import Flask, render_template, request
-from flask_debugtoolbar import DebugToolbarExtension
-from stories import story
+from flask import Flas, request, render_template, jsonify, session
+from boggle import Boggle
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
+app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
 
-debug = DebugToolbarExtension(app)
+boggle_game = Boggle()
 
 @app.route("/")
-def ask_questions():
-    """Generate ands how form to ask words"""
+def homepage():
+    """Show board."""
 
-    prompts = story.prompts
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
 
-    return render_template("questions.html", prompts = prompts)
+    return render_template("index.html", board=board, highscore=highscore, nplays=nplays)
 
-@app.route("/story")
-def show_story():
-    """Show story result"""
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
 
-    text = story.generate(request.args)
+    word = request.args["word"]
+    board = session["board"]
+    response =boggle_game.check_valid_word(board, word)
 
-    return render_template("story.html", text=text)
+    return jsonify({'result': response})
+
+@app.routes("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate"""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)                   
+
--- test.py
+++ test.py
@@ -1,22 +1,54 @@
+from unittest import TestCase
 from app import app
-import unittest
+from flask import session
+from boggle import Boggle
 
-class TestSimpleRoutes(unittest.TestCase):
 
-    def test_welcome(self):
-        tester = app.test_client(self)
-        response = tester.get('/welcome', content_type='html/text')
-        self.assertIn(b'welcome', response.data.lower())
-        self.assertEqual(response.status_code, 200)
+class FlaskTests(TestCase):
 
-    def test_welcome_home(self):
-        tester = app.test_client(self)
-        response = tester.get('/welcome/home', content_type='html/text')
-        self.assertIn(b'welcome home', response.data.lower())
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        """Stuff to do before every test."""
 
-    def test_welcome_back(self):
-        tester = app.test_client(self)
-        response = tester.get('/welcome/back', content_type='html/text')
-        self.assertIn(b'welcome back', response.data.lower())
-        self.assertEqual(response.status_code, 200)
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Make sure information is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'<p>High Score:', response.data)
+            self.assertIn(b'Score:', response.data)
+            self.assertIn(b'Seconds Left:', response.data)
+        
+    def test_valid_word(self):
+        """Test if word is valid by modifying the board in the session"""
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+               sess['board'] = [["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=cat')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_invalid_word(self):
+        """Test if word is in the dictionary"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    def non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get(
+            '/check-word?word=fsjdakfkldsfjdslkfjdlksf')
+        self.assertEqual(response.json['result'], 'not-word')
+
